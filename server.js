2ad67eb75826de01ba0395546de00ea44dd757f1
Raihan Aryanova <raihanaryanova@email.com>
2025-05-25 23:21:13 +0700

    footer

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
 // Routing
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "index.html"));
+  res.sendFile(path.join(__dirname, "pages", "HALAMAN_UTAMA.html"));
 });
 app.get("/top-up", (req, res) => {
   res.sendFile(path.join(__dirname, "pages", "top-up.html"));
@@ -23,12 +23,20 @@ app.get("/payment", (req, res) => {
 app.get("/list-game", (req, res) => {
   res.sendFile(path.join(__dirname, "pages", "list-game.html"));
 });
-app.get("/artikel", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "artikel.html"));
+app.get("/artikel-home", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "artikel-home.html"));
 });
 app.get("/news", (req, res) => {
   res.sendFile(path.join(__dirname, "pages", "news-home.html"));
 });
+app.get("/HALAMAN_UTAMA", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "artikel-home.html"));
+});
+
+app.get("/cek", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "cek-pesanan.html"));
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server nyala di http://localhost:${PORT}`);
