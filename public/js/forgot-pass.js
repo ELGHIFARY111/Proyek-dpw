0d406d6dec2962874c9b56f121126313babc0061
lucathy-475 <rendiyulistiof@gmail.com>
2025-05-25 17:55:42 +0700

    js forgot password ubah

--- public/js/forgot-pass.js
+++ public/js/forgot-pass.js
@@ -1,46 +1,23 @@
-// window.onload = function () {
-//     const form = document.getElementById('forgot-password');
-
-//     form.addEventListener('submit', function (event) {
-//     event.preventDefault();
-
-//     const email = document.getElementById('email').value.trim();
-//     const newPassword = document.getElementById('new').value.trim();
-
-//     let users = JSON.parse(localStorage.getItem('users')) || [];
-
-//     const userIndex = users.findIndex(user => user.email === email);
-
-//     if (userIndex !== -1) {
-//         users[userIndex].password = newPassword;
-//         localStorage.setItem('users', JSON.stringify(users));
-//         alert('Password berhasil diubah!');
-//         form.reset();
-//     } else {
-//         alert('Email tidak ditemukan!');
-//     }
-//     });
-// };
 window.onload = function () {
     const form = document.getElementById('forgot-password');
 
     form.addEventListener('submit', function (event) {
-        event.preventDefault();
+    event.preventDefault();
 
-        const email = document.getElementById('email').value.trim();
-        const newPassword = document.getElementById('new').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const newPassword = document.getElementById('new').value.trim();
 
-        let users = JSON.parse(localStorage.getItem('users')) || [];
+    let users = JSON.parse(localStorage.getItem('users')) || [];
 
-        const userIndex = users.findIndex(user => user.email === email);
+    const userIndex = users.findIndex(user => user.email === email);
 
-        if (userIndex !== -1) {
+    if (userIndex !== -1) {
         users[userIndex].password = newPassword;
         localStorage.setItem('users', JSON.stringify(users));
         alert('Password berhasil diubah!');
         form.reset();
-        } else {
+    } else {
         alert('Email tidak ditemukan!');
-        }
+    }
     });
-    };
+};
